67092ace2e11bbbe8ea27c2a28816a44e581caec
Aidan Campbell <aidan.campbell@esrf.fr>
2018-10-19 18:18:17 +0200

    Added catch-all url to redirect to home if the url does not exist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import {RemoteDesktopViewerComponent} from './remote-desktop-viewer/remote-desktop-viewer.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {RemoteDesktopDashboardComponent} from './remote-desktop-dashboard/remote-desktop-dashboard.component';
 import {JupyterhubComponent} from './jupyterhub/jupyterhub.component';
 import {VirtualMachinesComponent} from './virtual-machines/virtual-machines.component';
-import {AppComponent} from './app.component';
 import {AdminDashboardComponent} from './admin/admin-dashboard/admin-dashboard.component';
 
 import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
   {path: 'machine-view/:ip', component: RemoteDesktopViewerComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'remote-desktop', component: RemoteDesktopDashboardComponent},
   {path: 'jupyterhub', component: JupyterhubComponent},
   {path: 'virtual-machines', component: VirtualMachinesComponent},
   {path: 'admin', component: AdminDashboardComponent},
-
-
+  {path: '**', component: HomeComponent},
   {path: '', component: HomeComponent},
   // TODO  {path: '**', component: PageNotFound}
 
